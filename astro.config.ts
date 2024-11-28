2d92f46d07ca0aea03ebd629c1441628e4081e77
Sylfare <12037446+Sylfare@users.noreply.github.com>
2024-11-28 18:50:40 +0100

    feature: cacher les éléments de fenêtre sur les thèmes sans fenêtres

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    vite:{
+        build:{
+            assetsInlineLimit: 1024,
+        },
+    },
+    scopedStyleStrategy: "where",
     experimental: {
         contentIntellisense: true
     }
