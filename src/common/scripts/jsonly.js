28152e963fd94bbebf97c5af98df162836094a77
Sylfare <12037446+Sylfare@users.noreply.github.com>
2025-08-15 19:02:44 +0200

    fix: hopefully fix JsOnly + astro version bump

--- src/common/scripts/jsonly.js
+++ src/common/scripts/jsonly.js
@@ -6,18 +6,7 @@ function checkOnlyJs(node = document.body) {
         const nouveauBloc = document.createElement("div");
         parent?.replaceChild(nouveauBloc, element);
         nouveauBloc.outerHTML = contenu;
-        if(nouveauBloc.querySelector('script[type="text/html"]') != null) {
-            checkOnlyJs(nouveauBloc);
-        }
     });
     document.dispatchEvent(event);
 }
-document.addEventListener("readystatechange", e => checkOnlyJs());
-
-Object.keys(document).forEach(key => {
-    if (/^on/.test(key)) {
-        document.addEventListener(key.slice(2), event => {
-            console.log(event);
-        });
-    }
-});
+document.addEventListener("DOMContentLoaded", e => checkOnlyJs());
