09b163fe547291cef00f857ff008e689589fd6b0
Sylfare <12037446+Sylfare@users.noreply.github.com>
2026-05-08 19:22:55 +0200

    feature: more footer banners

--- src/css/plugins/assets.js
+++ src/css/plugins/assets.js
@@ -3,15 +3,15 @@ const vite = require("vite");
 module.exports = {
     
     install: function(less, pluginManager, functions) {
-        // console.log(process.env)
         const a = vite.loadEnv(process.env.NODE_ENV,  process.cwd(), "");
         
         functions.add('pi', function() {
             return new tree.Dimension(Math.PI);
         });
 
+        // deprecated, because everything is on the same domain now
         functions.add('asset', function(lien){
-            lien.value = `${a.ASSETS_BASE_URL}${lien.value}`;
+            lien.value = `${lien.value}`;
             return new tree.URL(lien);
         })
     }
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,5 +1,7 @@
+import { getRelativeLocaleUrl } from "astro:i18n";
 import { ui, defaultLang } from "./ui";
 import type { i18nKeys, i18nComplete } from "./ui";
+
 
 export function getLanguage(rawLang?: string) {
     return rawLang as keyof typeof ui ?? defaultLang;
@@ -8,6 +10,10 @@ export function getLanguage(rawLang?: string) {
 export function useTranslations(lang?: string) {
     const foundLang = getLanguage(lang);
     return (getKey: (keys: i18nKeys) => string | undefined) => getKey(ui[foundLang]) ?? getKey(ui[defaultLang]);
+}
+
+export function initUrl(language?: string) {
+    return (to?: string) => getRelativeLocaleUrl(getLanguage(language), to);
 }
 
 export type Buttons = keyof i18nComplete["buttons"];
